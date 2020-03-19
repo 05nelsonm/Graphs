0b2ec7b77f6d5a34b9ab89d5624a961653f6f9ee
Matthew Nelson <05nelsonm@protonmail.com>
2020-03-19 12:37:53 -0400

    complete Day 2 assignment 'ancestor.py'

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,31 @@
+import sys
+sys.path.append('../graph')
+from graph import Graph
+from util import Queue, Stack
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    flat_ancestors = {item for sublist in ancestors for item in sublist}
+    graph = Graph()
+
+    for item in flat_ancestors:
+        graph.add_vertex(item)
+
+    for ancestor in ancestors:
+        graph.add_edge(ancestor[1], ancestor[0])
+
+    stack = Stack()
+    stack.push(starting_node)
+    visited = []
+
+    while stack.size() > 0:
+        node = stack.pop()
+        for items in sorted(graph.vertices[node]):
+            stack.push(items)
+
+        if node not in visited and node != starting_node:
+            visited.append(node)
+
+    if len(visited) < 1:
+        return -1
+    return visited[-1]
